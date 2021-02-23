6e40690dcc5f39d675448666b0f3ba8a98724462
instateofgrace <spokenobject036@gmail.com>
2021-02-23 16:55:06 +0000

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
-
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+    char tmp;
+    int i = 0;
+    int size = strlen(str);
+    size--;
+    while (i <= size) {
+        tmp = str[i];
+        str[i] = str[size];
+        str[size] = tmp;
+        size--;
+        i++;
+    }
 }
-
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -3,4 +3,8 @@
 void Swap(char *left, char *right)
 {
 	// ваш код здесь
+    char tmp;
+    tmp=*left;
+    *left=*right;
+    *right=tmp;
 }
